f1e299eedfe47e4402f19ea247f9e1487509e6b3
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-05-27 17:13:24 +0800

    Auth form validation

--- src/features/auth/actions/signInAction.ts
+++ src/features/auth/actions/signInAction.ts
@@ -1,10 +1,12 @@
 "use server";
 
 import type { SignInFormData } from "@/features/auth/hooks/useSignIn";
+import signInSchema from "@/features/auth/schemas/signInSchema";
 import { toActionError } from "@/utils/helpers";
 
 const signInAction = async (formData: SignInFormData) => {
   try {
+    const validatedData = signInSchema.parse(formData);
   } catch (error) {
     return toActionError(error);
   }
--- src/features/auth/actions/signUpAction.ts
+++ src/features/auth/actions/signUpAction.ts
@@ -1,10 +1,12 @@
 "use server";
 
 import type { SignUpFormData } from "@/features/auth/hooks/useSignUp";
+import signUpSchema from "@/features/auth/schemas/signUpSchema";
 import { toActionError } from "@/utils/helpers";
 
 const signUpAction = async (formData: SignUpFormData) => {
   try {
+    const validatedData = signUpSchema.parse(formData);
   } catch (error) {
     return toActionError(error);
   }
