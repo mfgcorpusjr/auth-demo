fb3a40af9a3d8def983f4bfc27f6dcd48ce5f892
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-05-27 16:51:37 +0800

    Helper function

---
+++ src/utils/helpers.ts
@@ -0,0 +1,11 @@
+import { ZodError } from "zod";
+
+export const toActionError = (error: unknown) => {
+  if (error instanceof ZodError) {
+    return { success: false, message: "Validation failed" };
+  } else if (error instanceof Error) {
+    return { success: false, message: error.message };
+  } else {
+    return { success: false, message: "An unknown error has occurred" };
+  }
+};
