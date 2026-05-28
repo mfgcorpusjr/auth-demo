762fe71275c2a8084ff3d0e09f87145da74b5e6d
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-05-28 09:31:31 +0800

    Refactor proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,17 +4,20 @@ import { NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 
 const protectedRoutes = ["/dashboard"];
+const authRoutes = ["/auth"];
 
 export async function proxy(req: NextRequest) {
   const session = await auth.api.getSession({
     headers: req.headers,
   });
 
-  if (!session && protectedRoutes.includes(req.nextUrl.pathname)) {
+  const nextUrl = req.nextUrl.pathname;
+
+  if (!session && protectedRoutes.includes(nextUrl)) {
     return NextResponse.redirect(new URL("/auth", req.url));
   }
 
-  if (session && req.nextUrl.pathname === "/auth") {
+  if (session && authRoutes.includes(nextUrl)) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
